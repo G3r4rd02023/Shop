4e2159dcbe1669d4970f58b4dfd7bae9147a4dca
gerardo lanza <glanza007@gmail.com>
2020-10-02 12:10:08 -0600

    Publish Azure

--- Shop.Web/Startup.cs
+++ Shop.Web/Startup.cs
@@ -104,7 +104,7 @@ namespace Shop.Web
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Account}/{action=Login}/{id?}");
             });
         }
     }
